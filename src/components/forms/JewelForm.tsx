d070a2463d09199caaee04e191362a67ec38adfe
gabrielteodoroo <biel.teodorob@gmail.com>
2025-07-26 17:24:03 -0300

    chore: remove unused imports

--- src/components/forms/JewelForm.tsx
+++ src/components/forms/JewelForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef} from 'react';
+import  { useState, useEffect, useRef} from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useParams, useNavigate } from 'react-router-dom';
